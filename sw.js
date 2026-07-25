b918666f72e97a94dbbca02bcacf00b36492c39b
rastinzia97-eng <rastin.zia97@gmail.com>
2026-07-25 21:42:02 +0330

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='raee-cafe-v18';
+const CACHE='raee-cafe-v19';
 const ASSETS=['./index.html','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)))});
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
